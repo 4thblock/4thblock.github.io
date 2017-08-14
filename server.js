f56ceda90d6aed9a2f729c063d8ea5a45075ab6d
Prokop <prokopenko.serhii@gmail.com>
2017-08-14 03:55:40 +0300

    title update

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express   =     require("express");
 var app       =     express();
 var http = require('http').Server(app);
+var server = http.createServer(app);
+app.use(express.static(__dirname + '/static'));
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html'); 
     console.log('go');
 });
-
-app.use(express.static(__dirname + '/static'));
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-
 
 http.listen(app.get('port'), app.get('ip'), function () {
     console.log('Listening on port ' + app.get('port'));
