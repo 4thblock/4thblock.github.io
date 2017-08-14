364b1d27ed114cfa47faad4ab5a20f711038bae1
Prokop <prokopenko.serhii@gmail.com>
2017-08-14 04:40:28 +0300

    changed server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express   =     require("express");
 var app       =     express();
 var http = require('http').Server(app);
-var server = http.createServer(app);
+// var server = http.createServer(app);
 app.use(express.static(__dirname + '/static'));
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
